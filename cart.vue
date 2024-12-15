715cd71e3754c7a7f1c9125507fab4f6b0bc1a8b
MelinaMacias5 <137859778+MelinaMacias5@users.noreply.github.com>
2024-12-15 15:52:34 -0500

    Create cart.vue

---
+++ cart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Carrito de Compras</h1>
+    <ul id="cartItems">
+      <li v-for="(product, index) in products" :key="index">
+        {{ product.name }} - Precio: ${{ product.price.toFixed(2) }} - Descripción: {{ product.description }}
+        <button @click="removeProduct(index)">Eliminar</button>
+      </li>
+    </ul>
+    <p v-if="products.length === 0">No hay productos en el carrito.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cart',
+  data() {
+    return {
+      products: JSON.parse(localStorage.getItem('cartProducts')) || [],
+    };
+  },
+  methods: {
+    removeProduct(index) {
+      this.products.splice(index, 1);
+      localStorage.setItem('cartProducts', JSON.stringify(this.products));
+    },
+  },
+};
+</script>
+

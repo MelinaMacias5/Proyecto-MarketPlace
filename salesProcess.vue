ef9f3a2e58e45d12468da82280b26e297e70a008
MelinaMacias5 <137859778+MelinaMacias5@users.noreply.github.com>
2024-12-15 15:54:31 -0500

    Create salesProcess.vue

---
+++ salesProcess.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Proceso de Ventas</h1>
+    <p>Aquí puedes gestionar el proceso de ventas y ver el historial de compras.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'salesProcess',
+};
+</script>

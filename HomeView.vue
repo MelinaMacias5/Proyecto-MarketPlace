39f8dbddf01a846f523438c2b15affd98daed672
MelinaMacias5 <137859778+MelinaMacias5@users.noreply.github.com>
2024-12-15 15:54:03 -0500

    Create HomeView.vue

---
+++ HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Página de Inicio</h1>
+    <p>Bienvenido a nuestro Marketplace. Explora nuestros productos y realiza tus compras.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView', // Cambia el nombre aquí también
+};
+</script>

250e94d793bcba56c2016a0acccfdee69959c34d
MelinaMacias5 <137859778+MelinaMacias5@users.noreply.github.com>
2024-12-15 15:51:54 -0500

    Create addProduct.vue

---
+++ addProduct.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Agregar Producto</h1>
+    <form @submit.prevent="addProduct">
+      <input v-model="productName" placeholder="Nombre del Producto" required />
+      <input v-model="productPrice" type="number" placeholder="Precio" required />
+      <textarea v-model="productDescription" placeholder="Descripción" required></textarea>
+      <button type="submit">Agregar Producto</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'addProduct',
+  data() {
+    return {
+      productName: '',
+      productPrice: '',
+      productDescription: '',
+    };
+  },
+  methods: {
+    addProduct() {
+      const newProduct = {
+        name: this.productName,
+        price: parseFloat(this.productPrice),
+        description: this.productDescription,
+      };
+      let products = JSON.parse(localStorage.getItem('cartProducts')) || [];
+      products.push(newProduct);
+      localStorage.setItem('cartProducts', JSON.stringify(products));
+      alert('Producto agregado exitosamente.');
+      this.productName = '';
+      this.productPrice = '';
+      this.productDescription = '';
+    },
+  },
+};
+</script>
+
